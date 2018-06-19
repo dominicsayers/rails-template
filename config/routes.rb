40997b3f4bc1e0e26b0b166f4473d3db3396ea02
Dominic Sayers <dominic@sayers.cc>
2018-06-19 17:04:41 +0100

    Add authentication page components

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  resources :users
+
+  resources :users do
+    get 'public', on: :member
+  end
 end
