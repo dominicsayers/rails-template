1f4a2684ec09b9a9416697e17a5e5b3bfa5ef959
Dominic Sayers <dominic@sayers.cc>
2018-07-27 17:15:19 +0100

    Limit accounts access to logged-in users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :accounts
+
+  authenticate :user do
+    resources :accounts
+  end
 
   resources :users do
     get 'public', on: :member
